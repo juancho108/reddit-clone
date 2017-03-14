12382e8e090aaee9f75aae01934d9cecde768d23
juancho108 <jjf108@gmail.com>
2017-03-14 14:21:51 -0300

    agregando todo

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	
+	<meta charset="utf-8">
+	<title>Reddit clone</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+	<div class="container">
+	
+		<div class="row">
+			
+			<div class=" col-md-12">
+				
+				<h1>Reddit Clone</h1>
+
+			</div>
+
+		</div>
+
+		<hr>
+
+	@yield('content')
+
+	</div>
+
+</body>
+
+
+</html>
